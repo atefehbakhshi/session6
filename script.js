719201b70f8092f9b7eb1440ddd1fc505949802a
Atefeh <at.bakhshi1373@gmail.com>
2023-07-14 13:01:25 +0330

    feat: form and video

---
+++ script.js
@@ -0,0 +1,19 @@
+// const form = document.getElementById("submit-user");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const { username, lastname, phonNumber, email, date, gender, color, course } =
+//     e.target;
+//   console.log(gender);
+//   const userdata = {
+//     username: username.value,
+//     lastname: lastname.value,
+//     phonNumber: phonNumber.value,
+//     email: email.value,
+//     date: date.value,
+//     gender: gender.value,
+//     color: color.value,
+//     course: course.value,
+//   };
+//   console.log(userdata);
+// });
